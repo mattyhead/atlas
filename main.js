b823af133201bfc73a871a2cc7ec24251e6b610b
matt murphy <mattyhead@gmail.com>
2017-10-04 14:11:12 -0400

    ...

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ var GATEKEEPER_KEY = '35ae5b7bf8f0ff2613134935ce6b4c1e';
 var CITY_HALL = [39.95, -75.1642];
 
 var mymap = L.map('lmap').setView(CITY_HALL, 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
