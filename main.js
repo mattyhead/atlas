3f1bffd5aea801c3ffaa27ee17244808ca42a758
matt murphy <mattyhead@gmail.com>
2017-10-04 16:06:17 -0400

    ...

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-$.(function($, w, d) {
+// IIFE - Immediately Invoked Function Expression
+(function(yourcode) {
+
+  // The global jQuery object is passed as a parameter
+  yourcode(window.jQuery, window, document);
+
+}(function($, w, d) {
   var GATEKEEPER_KEY = '82fe014b6575b8c38b44235580bc8b11';
   var CITY_HALL = [39.95, -75.1642];
 
-  var lmap = L.map('lmap').setView(CITY_HALL, 13);
+  // The $ is now locally scoped 
+  $(function() {
 
-  //L.esri.basemapLayer('//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer').addTo(mymap);
-  /*        pwd: {
-            url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer',
-            tiledLayers: [
-              'cityBasemapLabels'
-            ],
-            type: 'featuremap'
-          },
+    // The DOM is ready!
+    var lmap = L.map('lmap').setView(CITY_HALL, 13);
 
-          //tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer
-  */
-  L.esri.tiledMapLayer({
-    url: "//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer"
-  }).addTo(lmap);
-  L.esri.tiledMapLayer({
-    url: "//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer"
-  }).addTo(lmap);
-})(jQuery, window, document)
+    //L.esri.basemapLayer('//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer').addTo(mymap);
+    /*        pwd: {
+                     url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer',
+                     tiledLayers: [
+                       'cityBasemapLabels'
+                     ],
+                     type: 'featuremap'
+                   },
+
+                   //tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer
+    */
+    L.esri.tiledMapLayer({
+      url: "//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer"
+    }).addTo(lmap);
+    L.esri.tiledMapLayer({
+      url: "//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer"
+    }).addTo(lmap);
+  });
+
+  // The rest of your code goes here!
+
+}));
