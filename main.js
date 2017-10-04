328c64a5c4b7bf457b06412d5df38672b22a4625
matt murphy <mattyhead@gmail.com>
2017-10-04 14:45:29 -0400

    ...

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ var CITY_HALL = [39.95, -75.1642];
 
 var mymap = L.map('lmap').setView(CITY_HALL, 13);
 
-L.esri.basemapLayer("Topographic").addTo(mymap);
+L.esri.basemapLayer('//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer').addTo(mymap);
 /*        pwd: {
           url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer',
           tiledLayers: [
             'cityBasemapLabels'
           ],
           type: 'featuremap'
-        },*/
+        },
+
+L.esri.tiledMapLayer({
+    url: "https://services.arcgisonline.com/ArcGIS/rest/services/USA_Topo_Maps/MapServer"
+  }).addTo(map);
+
+  */
