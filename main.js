a2929091da48c38a8b4dd3d62ffaa1e2d006b577
matt murphy <mattyhead@gmail.com>
2017-10-04 14:41:18 -0400

    ...

--- main.js
+++ main.js
@@ -3,14 +3,8 @@ var CITY_HALL = [39.95, -75.1642];
 
 var mymap = L.map('lmap').setView(CITY_HALL, 13);
 
-L.tileLayer('//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mymap',
-  accessToken: GATEKEEPER_KEY
-}).addTo(mymap);
-/*
-        pwd: {
+L.esri.basemapLayer("Topographic").addTo(map);
+/*        pwd: {
           url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer',
           tiledLayers: [
             'cityBasemapLabels'
