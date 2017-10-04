b5d0799a536b10b9e27a240c8bb30ce2b9ba80f5
matt murphy <mattyhead@gmail.com>
2017-10-04 14:07:55 -0400

    ...

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var GATEKEEPER_KEY = '35ae5b7bf8f0ff2613134935ce6b4c1e';
 var CITY_HALL = [39.95, -75.1642];
 
-var mymap = L.map('lmap').setView([51.505, -0.09], 13);
+var mymap = L.map('lmap').setView(CITY_HALL, 13);
