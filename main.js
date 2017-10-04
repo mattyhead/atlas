3c4875760946307fbde424fdbe3ba89a4599b357
matt murphy <mattyhead@gmail.com>
2017-10-04 16:12:09 -0400

    ...

--- main.js
+++ main.js
@@ -7,12 +7,13 @@
 }(function($, w, d) {
   var GATEKEEPER_KEY = '82fe014b6575b8c38b44235580bc8b11';
   var CITY_HALL = [39.95262, -75.16422];
+  var ZOOM = 16
 
   // The $ is now locally scoped 
   $(function() {
 
     // The DOM is ready!
-    var lmap = L.map('lmap').setView(CITY_HALL, 18);
+    var lmap = L.map('lmap').setView(CITY_HALL, ZOOM);
 
     //L.esri.basemapLayer('//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer').addTo(mymap);
     /*        pwd: {
