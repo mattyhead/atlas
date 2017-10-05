81a0115ab6a21c20fb80f83342103e7fef92473b
matt murphy <mattyhead@gmail.com>
2017-10-04 20:59:55 -0400

    ...

--- main.js
+++ main.js
@@ -87,19 +87,19 @@
     }).addTo(lmap);
 
     // test markers
-    markers.home = L.marker([39.95261, -75.16422], {
+    markers.home = L.marker([39.9521, -75.16422], {
       icon: homeIcon
     }).addTo(lmap);
-    markers.polling = L.marker([39.95263, -75.16422], {
+    markers.polling = L.marker([39.9523, -75.16422], {
       icon: pollingIcon
     }).addTo(lmap);
-    markers.congress = L.marker([39.95265, -75.16422], {
+    markers.congress = L.marker([39.9525, -75.16422], {
       icon: congressIcon
     }).addTo(lmap);
-    markers.entrance = L.marker([39.95267, -75.16422], {
+    markers.entrance = L.marker([39.9527, -75.16422], {
       icon: entranceIcon
     }).addTo(lmap);
-    markers.handi = L.marker([39.95269, -75.16422], {
+    markers.handi = L.marker([39.9529, -75.16422], {
       icon: handiIcon
     }).addTo(lmap);
 
