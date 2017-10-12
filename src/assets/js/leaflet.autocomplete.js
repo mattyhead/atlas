18e4daf41a30925852ae6b71a20228f6cfa43529
matt murphy <mattyhead@gmail.com>
2017-10-12 15:54:53 -0400

    ..

--- src/assets/js/leaflet.autocomplete.js
+++ src/assets/js/leaflet.autocomplete.js
@@ -122,7 +122,7 @@
         corner.appendChild(container)
       }
 
-      L.DomEvent.addListener(this.searchBox, 'load', function() {
+      L.DomEvent.addListener(this.searchBox, 'onload', function() {
         AC();
       })
 
