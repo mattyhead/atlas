0da6774480336b1289d52c2304b19b235c32c695
matthew murphy <mattyhead@gmail.com>
2017-10-14 19:41:03 -0400

    ...

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,7 +15,7 @@
         })
         .addTo(map);*/
 
-    new L.Control.Autocomplete().addTo(lmap).setService(W.AC);
+    new L.Control.SearchBox().addTo(lmap).setService(W.AC);
 
     // set up layers
     L.esri.tiledMapLayer({
