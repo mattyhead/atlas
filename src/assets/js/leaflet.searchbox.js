7b41e75de9e0e70fdf3b7e854aefd2f838625d3e
matthew murphy <mattyhead@gmail.com>
2017-10-14 20:51:19 -0400

    ...

--- src/assets/js/leaflet.searchbox.js
+++ src/assets/js/leaflet.searchbox.js
@@ -120,7 +120,7 @@
       // crude but effective event cludge
       console.log(this.searchBox)
       L.DomEvent.addListener(this.searchBox, 'keyup', function() {
-        //service(this)
+        AC(this)
         console.log(this)
         L.DomEvent.removeListener(this, 'keyup')
       })
