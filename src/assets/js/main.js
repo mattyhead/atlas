a13d1fe0b4e2ed99d9dcbe1247af68312e2a61d7
matt murphy <mattyhead@gmail.com>
2017-10-17 16:25:04 -0400

    ...

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -254,9 +254,7 @@
     function getDivisionShape(input) {
         var deferred = $.Deferred(),
             service = services.division_shape
-        console.log(service.url(input), input)
         $.getJSON(service.url(input), service.params).done(function(response) {
-            console.log(response)
             if (response.features) {
                 deferred.resolve({
                     coordinates: response.features[0].geometry.rings[0],
