d8a6aafba97a97c9514c0e2b8ea15ce19b6d48d2
matt murphy <mattyhead@gmail.com>
2017-10-19 15:41:39 -0400

    ...

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -214,9 +214,9 @@
             ds.coordinates = coordsSwap(ds.coordinates)
                 //            ds.marker = L.polygon(ds.coordinates, ds.style).addTo(lmap)
             ds.marker = L.polygon(ds.coordinates, ds.style).bindTooltip(ds.name, {
-                permanant: true,
+                /*permanant: true,
                 className: "polygon-labels",
-                offset: [0, 0]
+                offset: [0, 0]*/
             }).addTo(lmap).openTooltip()
 
             var center = ds.marker.getBounds().getCenter()
