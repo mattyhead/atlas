b6ff242c1f4746f87fe30b209a794307630d8cd9
matthew murphy <mattyhead@gmail.com>
2017-10-19 21:12:34 -0400

    ...

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -228,7 +228,7 @@
 
             // draw info display
             $('#panel').html(
-              '<div id="accordion">' + 
+/*              '<div id="accordion">' + 
               '  <h3 >Polling Place</h3>' +
               '  <p>' +
               '    <div>' + pp.data.location + '</div>' +
@@ -247,8 +247,33 @@
               '  <p>' +
               '    <div>Other stuff</div>' +
               '  </p>' +
-              '</div>'
-              )
+              '</div>'*/
+
+'<div id="accordion">
+  <h3>Section 1</h3>
+  <div>
+    <p>Mauris mauris ante, blandit et, ultrices a, suscipit eget.
+    Integer ut neque. Vivamus nisi metus, molestie vel, gravida in,
+    condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros.
+    Nam mi. Proin viverra leo ut odio.</p>
+  </div>
+  <h3>Section 2</h3>
+  <div>
+    <p>Sed non urna. Phasellus eu ligula. Vestibulum sit amet purus.
+    Vivamus hendrerit, dolor aliquet laoreet, mauris turpis velit,
+    faucibus interdum tellus libero ac justo.</p>
+  </div>
+  <h3>Section 3</h3>
+  <div>
+    <p>Nam enim risus, molestie et, porta ac, aliquam ac, risus.
+    Quisque lobortis.Phasellus pellentesque purus in massa.</p>
+    <ul>
+      <li>List item one</li>
+      <li>List item two</li>
+      <li>List item three</li>
+    </ul>
+  </div>
+</div>'              )
               $( "#accordion" ).accordion();
         })
 
